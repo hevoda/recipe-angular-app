30e83c75cb9ff21b6e784740486a54c3edcb9209
Hervé EVODA <herveevoda@gmail.com>
2023-06-22 20:26:15 +0200

    20230623 - Adding content to the recipe components .

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+   recipes: Recipe[] = [
+    new Recipe(' Test Recipe','This is simply a test','https://2.bp.blogspot.com/-VjMpIqEsMWY/TpQEbrGtS-I/AAAAAAAAAd8/CG-L0Ugph60/s1600/Koral+fish+fry.jpg')
+   ];
 }
